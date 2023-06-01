ff1a425b40005f6f61334eaa426439788b43381e
Christopher <christopheranchetaece@gmail.com>
2023-06-01 13:35:41 +1200

    issue with result showing all question

--- pages/views.py
+++ pages/views.py
@@ -5,16 +5,15 @@ import random
 
 def index(request):
     questions = list(Question.objects.all())
-    # random.shuffle(questions)
-    # random_questions = questions[:5]  # Change the number (5) to the desired number of random questions to display
+    random.shuffle(questions)
+    random_questions = questions[:5]  # Change the number (5) to the desired number of random questions to display
 
     context = {
-        'questions': questions
+        'questions': random_questions
     }
     return render(request, 'pages/index.html', context)
 
 from django.core.exceptions import ObjectDoesNotExist
-
 def submit_quiz(request):
     if request.method == 'POST':
         # Process submitted answers and calculate the score
@@ -22,31 +21,33 @@ def submit_quiz(request):
         answers = []
         total_questions = 0
 
-        total_questions = request.POST.get('total_questions')
-        # total_questions = int(total_questions_str)
-        print(total_questions)
+        total_questions_str = request.POST.get('total_questions')
+        total_questions = int(total_questions_str)
 
-        for question in Question.objects.all():
-            selected_option_id = request.POST.get(f'q{question.id}')
-            try:
-                selected_option = Option.objects.get(id=selected_option_id)
-            except ObjectDoesNotExist:
-                # Handle the case when the Option object does not exist
-                # You can display an error message or handle it based on your requirements
-                selected_option = None
+        for question_id in request.POST.keys():
+            if question_id.startswith('q'):
+                selected_option_id = request.POST.get(question_id)
+                try:
+                    question = Question.objects.get(id=question_id[1:])
+                    selected_option = Option.objects.get(id=selected_option_id)
+                except ObjectDoesNotExist:
+                    # Handle the case when the Question or Option object does not exist
+                    # You can display an error message or handle it based on your requirements
+                    question = None
+                    selected_option = None
 
-            if selected_option and selected_option.is_correct:
-                score += 1
+                if question and selected_option and selected_option.is_correct:
+                    score += 1
 
-            answers.append({
-                'question': question.text,
-                'selected_option': selected_option.text if selected_option else 'N/A',
-                'is_correct': selected_option.is_correct if selected_option else False,
-                'explanation': question.explanation
-            })
+                answers.append({
+                    'question': question.text if question else 'N/A',
+                    'selected_option': selected_option.text if selected_option else 'N/A',
+                    'is_correct': selected_option.is_correct if selected_option else False,
+                    'explanation': question.explanation if question else 'N/A'
+                })
 
         # Calculate the score percentage
-        score_percentage = round((score / int(total_questions)) * 100, 2)
+        score_percentage = round((score / total_questions) * 100, 2)
         context = {
             'score': score,
             'total_questions': total_questions,
@@ -57,9 +58,6 @@ def submit_quiz(request):
 
     else:
         return redirect('index')
-
-
-
 
 
 
